8e349e97f866c31bb7534cc4b91469f6876f1329
Joey <Fangjin98@outlook.com>
2022-02-21 21:04:23 +0800

    maintain hosts and switches of json file.

--- routing/utils/TopoGenerator.py
+++ routing/utils/TopoGenerator.py
@@ -1,10 +1,33 @@
 import json
+import random
+
 from collections import defaultdict
-from os import link
+
+def get_link_array(paths: list , topo_dict: dict):
+    link_index=defaultdict(dict)
+    link_num=0
+    band=[]
+    
+    for p in paths:
+        weight_p=p.get_link_weight(topo_dict)
+        for l in p.link_list:
+            if l[1] not in link_index[l[0]].keys():
+                link_index[l[0]][l[1]]=link_num
+                link_num+=1
+                band.append(weight_p[l[0]][l[1]])
+    
+    return link_index,band,link_num
 
 class TopoGenerator(object):
     def __init__(self, topo_dict=defaultdict(list)):
         self.topo_dict = topo_dict
+        self.host_set=[]
+        self.switch_set=[]
+        for key in self.topo_dict.keys():
+            if key[0]=='v':
+                self.switch_set.append(key)
+            elif key[0]=='h':
+                self.host_set.append(key)
     
     def __str__(self) -> str:
         return str(self.topo_dict)
@@ -24,7 +47,7 @@ class TopoGenerator(object):
                     self.topo_dict[src].remove(node)
                     break
     
-    def construct_path_set(self,src_set,dst_set,max_len=3):
+    def construct_path_set(self,src_set,dst_set,max_len=5):
         path=defaultdict(dict)
         for s in src_set:
             for d in dst_set:
@@ -57,6 +80,37 @@ class TopoGenerator(object):
         with open(json_file, 'w') as f:
             f.write(json_str)
 
+    def generate_test_set(self, worker_num, switch_num,random_pick=False,flatten=True):
+        worker_num_of_rack=int(worker_num/switch_num)
+        if not random_pick: # TODO: need to check replication of random pick
+            ps=self.host_set[0]
+        else:
+            ps=random.choice(self.host_set)
+        worker_set=[]
+        switch_set=[]
+        
+        for i in range(switch_num):
+            if not random_pick:
+                switch_set.append(self.switch_set[i])
+            else:
+                switch_set.append(random.choice(self.switch_set)) 
+        if flatten: # for RRIAR
+            for i in range(1,worker_num+1):
+                if not random_pick:
+                    worker_set.append(self.host_set[i])
+                else:
+                    worker_set.append(random.choice(self.host_set))
+        else: # for ATP & SwitchML
+            for i in range(switch_num):
+                rack_of_worker_set=[]
+                for j in range(worker_num_of_rack):
+                    if not random_pick:
+                        rack_of_worker_set.append(self.host_set[1+i*worker_num_of_rack+j])
+                    else:
+                        rack_of_worker_set.append(random.choice(self.host_set))
+                worker_set.append(rack_of_worker_set)
+                
+        return [ps, worker_set, switch_set]
 
 class Path(object):
     def __init__(self, node_list,link_list=None):
@@ -83,13 +137,3 @@ class Path(object):
             node2=self.node_list[i+1]
             link_weight[node1][node2]=topo_dict[node1][node2]
         return link_weight
-
-
-if __name__ == '__main__':
-    topo=TopoGenerator(json.load(open('/home/sdn/fj/distributed_PS_ML/routing/data/topo/fattree80.json')))
-    host_set=['h1','h2','h3']
-    switch_set=['v1','v2','v3']
-    path=topo.construct_path_set(host_set,switch_set,6)
-    print(path[host_set[1]][switch_set[1]][0])
-    print(path[host_set[1]][switch_set[1]][0].get_link(topo.topo_dict))
-    
