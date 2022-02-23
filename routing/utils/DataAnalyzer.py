dfe5d89c4108501103729f374bccd8d764b4aaa7
Joey <Fangjin98@outlook.com>
2022-02-23 11:18:47 +0800

    add train time analysis

--- routing/utils/DataAnalyzer.py
+++ routing/utils/DataAnalyzer.py
@@ -17,13 +17,27 @@ class DataAnalyzer:
                 throughput+=t
         return throughput
     
-    def cal_train_time(self,aggregation_points, t, comp, band, iteration=100) -> float:
+    def cal_train_time(self,worker_set, switch_set, aggregation_points,routing_paths, t, comp, ps_comp, band, iteration=100) -> float:
         train_time=0
+        aggregated_switch=[]
+        # computation time
+        for w in worker_set:
+            n=aggregation_points[w]
+            if n in switch_set:
+                train_time+=(t/comp)
+                if n not in aggregated_switch:
+                    aggregated_switch.append(n)
+            else:
+                train_time+=(t/ps_comp)
+        train_time+=(t*len(aggregated_switch)/ps_comp)
+        # transfer time
+        for p in routing_paths:
+            for l in p.link_list:
+                train_time+=(t/band)
+        
+        return train_time*iteration
 
 
 class SwitchMLDataAnalyzer(DataAnalyzer):
     def __init__(self, title: String, topo: TopoGenerator) -> None:
         super().__init__(title, topo)
-    
-    def cal_throughput(self, ps, worker_set, switch_set, aggregation_points, t) -> float:
-        return super().cal_throughput(ps, worker_set, switch_set, aggregation_points, t)
