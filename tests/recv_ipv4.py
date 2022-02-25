7e98a91989f51e8eed1a05228921844a52a0a5ef
Joey <FangJin98@outlook.com>
2022-02-25 17:29:31 +0800

    move files to scripts

--- tests/recv_ipv4.py
+++
@@ -1,9 +0,0 @@
-from scapy.all import *
-
-
-iface = "eno6"
-
-if __name__=="__main__":
-    print("Sniffing on ", iface)
-    print("Press Ctrl-C to stop...")
-    sniff(iface=iface, prn=lambda p: p.show())
