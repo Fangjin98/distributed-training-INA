79fba985a44b68e168c86e4ae65979a0eeb8de36
Joey <fangjin98@outlook.com>
2023-11-09 13:30:18 +0800

    fix import errors

--- src/common/utils.py
+++ src/common/utils.py
@@ -8,16 +8,22 @@ from multiprocessing import Process
 from time import sleep
 import time
 
-from src.comm_libs.header_config import DATA_NUM, DATA_BYTE, HEADER_BYTE
+from src.common.header_config import DATA_NUM, DATA_BYTE, HEADER_BYTE
+
 
 def is_port_in_use(port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        return s.connect_ex(('localhost', port)) == 0
+        return s.connect_ex(("localhost", port)) == 0
+
 
 def killport(port):
-    command = '''kill -9 $(netstat -nlp | grep :''' + str(
-        port) + ''' | awk '{print $7}' | awk -F"/" '{ print $1 }')'''
+    command = (
+        """kill -9 $(netstat -nlp | grep :"""
+        + str(port)
+        + """ | awk '{print $7}' | awk -F"/" '{ print $1 }')"""
+    )
     os.system(command)
+
 
 def bind_port(listen_ip, listen_port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -37,11 +43,13 @@ def bind_port(listen_ip, listen_port):
     conn, _ = s.accept()
     return conn
 
+
 def send_timestamp_data(s, timestamp, data):
     s.sendall(struct.pack(">d", timestamp))
     ser_data = pickle.dumps(data)
     s.sendall(struct.pack(">I", len(ser_data)))
     s.sendall(ser_data)
+
 
 def get_data(s):
     data_len = struct.unpack(">I", s.recv(4))[0]
@@ -49,10 +57,12 @@ def get_data(s):
     recv_data = pickle.loads(data)
     return recv_data
 
+
 def get_data_from_nic(s, buffer):
     while True:
         raw_data = s.recvfrom(HEADER_BYTE + DATA_BYTE)[0]
         buffer.append(raw_data)
+
 
 class RecvThread(threading.Thread):
     def __init__(self, func, args=()):
--- src/distributed_training/launch.py
+++ src/distributed_training/launch.py
@@ -13,39 +13,44 @@ import os
 from utils import datasets
 from utils import models
 from utils import worker
-from comm_libs.utils import get_data, bind_port, send_timestamp_data
+from common.utils import get_data, bind_port, send_timestamp_data
 
-parser = argparse.ArgumentParser(description='Distributed Model Training')
-parser.add_argument('--idx', type=int, default=0)
-parser.add_argument('--master', type=int)
-parser.add_argument('--master_ip', type=str, default='127.0.0.1')
-parser.add_argument('--master_port', type=int, default=53300)
-parser.add_argument('--ip', type=str, default='127.0.0.1')
-parser.add_argument('--base_port', type=int, default=53300)
-parser.add_argument('--worker_num', type=int, default=8)
-parser.add_argument('--config_file', type = str, default="./config.json")
-parser.add_argument('--model', type=str, default='alexnet')
-parser.add_argument('--dataset', type=str, default='CIFAR10')
-parser.add_argument('--batch_size', type=int, default=64)
-parser.add_argument('--epoch', type=int, default=100)
+parser = argparse.ArgumentParser(description="Distributed Model Training")
+parser.add_argument("--idx", type=int, default=0)
+parser.add_argument("--master", type=int)
+parser.add_argument("--master_ip", type=str, default="127.0.0.1")
+parser.add_argument("--master_port", type=int, default=53300)
+parser.add_argument("--ip", type=str, default="127.0.0.1")
+parser.add_argument("--base_port", type=int, default=53300)
+parser.add_argument("--worker_num", type=int, default=8)
+parser.add_argument("--config_file", type=str, default="./config.json")
+parser.add_argument("--model", type=str, default="alexnet")
+parser.add_argument("--dataset", type=str, default="CIFAR10")
+parser.add_argument("--batch_size", type=int, default=64)
+parser.add_argument("--epoch", type=int, default=100)
 
 args = parser.parse_args()
 
 CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
-DATASET_PATH= CURRENT_PATH+'/../data/datasets'
+DATASET_PATH = CURRENT_PATH + "/../data/datasets"
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-ip=args.ip
+ip = args.ip
 
 
 def aggregate(global_model, worker_list, step_size):
     local_para = torch.nn.utils.parameters_to_vector(global_model.parameters())
-    
+
     average_weight = 1.0 / (len(worker_list) + 1)
-    local_para += average_weight * step_size * sum([w.updated_paras - local_para for w in worker_list])
-    
+    local_para += (
+        average_weight
+        * step_size
+        * sum([w.updated_paras - local_para for w in worker_list])
+    )
+
     torch.nn.utils.vector_to_parameters(local_para, global_model.parameters())
+
 
 def test(model, data_loader, device):
     model.eval()
@@ -60,7 +65,7 @@ def test(model, data_loader, device):
             data, target = data.to(device), target.to(device)
             output = model(data)
             # sum up batch loss
-            loss_func = torch.nn.CrossEntropyLoss(reduction='sum')
+            loss_func = torch.nn.CrossEntropyLoss(reduction="sum")
             loss += loss_func(output, target).item()
             # get the index of the max log-probability
             pred = output.argmax(1, keepdim=True)
@@ -72,12 +77,15 @@ def test(model, data_loader, device):
 
     return loss, accuracy
 
+
 def train(model, data_loader, optimizer, local_iters=None, device=None):
     model.train()
 
     if local_iters is None:
-        local_iters = math.ceil(len(data_loader.loader.dataset) / data_loader.loader.batch_size)
-    
+        local_iters = math.ceil(
+            len(data_loader.loader.dataset) / data_loader.loader.batch_size
+        )
+
     train_loss = 0.0
     samples_num = 0
 
@@ -86,12 +94,12 @@ def train(model, data_loader, optimizer, local_iters=None, device=None):
         data, target = data.to(device), target.to(device)
         output = model(data)
         optimizer.zero_grad()
-        loss_func=torch.nn.CrossEntropyLoss()
+        loss_func = torch.nn.CrossEntropyLoss()
         loss = loss_func(output, target)
         loss.backward()
         optimizer.step()
 
-        train_loss += (loss.item() * data.size(0))
+        train_loss += loss.item() * data.size(0)
         samples_num += data.size(0)
 
     if samples_num != 0:
@@ -99,12 +107,17 @@ def train(model, data_loader, optimizer, local_iters=None, device=None):
 
     return train_loss
 
-def communication_parallel(worker_list, action, para=None,updated_data=None, partition=None):
+
+def communication_parallel(
+    worker_list, action, para=None, updated_data=None, partition=None
+):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=len(worker_list), )
+    executor = concurrent.futures.ThreadPoolExecutor(
+        max_workers=len(worker_list),
+    )
     tasks = []
-    
+
     for w in worker_list:
         if action == "init":
             tasks.append(loop.run_in_executor(executor, w.launch, para, partition))
@@ -112,46 +125,47 @@ def communication_parallel(worker_list, action, para=None,updated_data=None, par
             tasks.append(loop.run_in_executor(executor, w.get_trained_model))
         elif action == "push":
             tasks.append(loop.run_in_executor(executor, w.send_data, updated_data))
-    
+
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close()
 
+
 def master_loop(model, dataset, worker_num, config_file, batch_size, epoch, base_port):
-    master_listen_port_base = base_port + random.randint(0,20) * 20
-    worker_list=[]
-    step_size=1
+    master_listen_port_base = base_port + random.randint(0, 20) * 20
+    worker_list = []
+    step_size = 1
 
     try:
         global_model = models.get_model(model)
     except TypeError as t:
         print(t)
         exit(1)
-        
+
     try:
-        train_dataset, test_dataset = datasets.load_dataset(dataset,DATASET_PATH)
+        train_dataset, test_dataset = datasets.load_dataset(dataset, DATASET_PATH)
     except TypeError as t:
         print(t)
         exit(1)
 
     file = open(config_file)
     try:
-        host_config=json.load(file)
+        host_config = json.load(file)
         for i in range(worker_num):
             worker_list.append(
                 worker.Worker(
-                    i, 
-                    dataset, 
-                    model, 
-                    True, 
-                    epoch, 
-                    batch_size, 
-                    host_config[i]['host_ip'], 
-                    host_config[i]['ssh_port'],
-                    ip, 
-                    master_listen_port_base+i,
-                    host_config[i]['work_dir'],
+                    i,
+                    dataset,
+                    model,
+                    True,
+                    epoch,
+                    batch_size,
+                    host_config[i]["host_ip"],
+                    host_config[i]["ssh_port"],
+                    ip,
+                    master_listen_port_base + i,
+                    host_config[i]["work_dir"],
                     host_config[i]["ssh_usr"],
-                    host_config[i]["ssh_psw"]
+                    host_config[i]["ssh_psw"],
                 )
             )
     except KeyError as e:
@@ -161,14 +175,27 @@ def master_loop(model, dataset, worker_num, config_file, batch_size, epoch, base
         file.close()
 
     init_para = torch.nn.utils.parameters_to_vector(global_model.parameters())
-    print("Model {}: {} paras, {} MB".format(model,init_para.nelement(),init_para.nelement() * init_para.element_size() / 1024 / 1024))
-    
-    test_loader = datasets.create_dataloaders(test_dataset, batch_size,shuffle=False)
-    train_data_partition, test_data_partition = datasets.partition_data(dataset, worker_num, train_dataset, test_dataset)
-    
+    print(
+        "Model {}: {} paras, {} MB".format(
+            model,
+            init_para.nelement(),
+            init_para.nelement() * init_para.element_size() / 1024 / 1024,
+        )
+    )
+
+    test_loader = datasets.create_dataloaders(test_dataset, batch_size, shuffle=False)
+    train_data_partition, test_data_partition = datasets.partition_data(
+        dataset, worker_num, train_dataset, test_dataset
+    )
+
     try:
         print("Launch workers and send init paras.")
-        communication_parallel(worker_list, action="init", para=init_para, partition=(train_data_partition, test_data_partition))
+        communication_parallel(
+            worker_list,
+            action="init",
+            para=init_para,
+            partition=(train_data_partition, test_data_partition),
+        )
     except Exception as e:
         print(e)
         for w in worker_list:
@@ -180,35 +207,50 @@ def master_loop(model, dataset, worker_num, config_file, batch_size, epoch, base
         global_model.to(device)
 
         for epoch_idx in range(epoch):
-            comm_duration=0
-            start_time=time.time()
+            comm_duration = 0
+            start_time = time.time()
             communication_parallel(worker_list, action="pull")
-            comm_duration+=(time.time()- max([w.sending_time for w in worker_list]))
-            delta_time = max([w.sending_time for w in worker_list]) - min([w.sending_time for w in worker_list])
+            comm_duration += time.time() - max([w.sending_time for w in worker_list])
+            delta_time = max([w.sending_time for w in worker_list]) - min(
+                [w.sending_time for w in worker_list]
+            )
 
-            start_agg=time.time()
+            start_agg = time.time()
             aggregate(global_model, worker_list, step_size)
-            agg_duration=time.time()-start_agg
+            agg_duration = time.time() - start_agg
 
-            updated_para = torch.nn.utils.parameters_to_vector(global_model.parameters()).detach()
-            communication_parallel(worker_list, action="push", updated_data=updated_para)
-            
+            updated_para = torch.nn.utils.parameters_to_vector(
+                global_model.parameters()
+            ).detach()
+            communication_parallel(
+                worker_list, action="push", updated_data=updated_para
+            )
+
             total_duration = time.time() - start_time
-            
+
             loss, acc = test(global_model, test_loader, device)
 
-            print("Epoch: {}, throughput = {} image/s, accuracy = {}, loss = {}.".format(epoch_idx, worker_num*batch_size/total_duration, acc, loss))
-            print("Total duration => {} sec, comm => {} sec, aggregation => {} sec, delta => {} sec".format(total_duration, comm_duration,agg_duration, delta_time))
+            print(
+                "Epoch: {}, throughput = {} image/s, accuracy = {}, loss = {}.".format(
+                    epoch_idx, worker_num * batch_size / total_duration, acc, loss
+                )
+            )
+            print(
+                "Total duration => {} sec, comm => {} sec, aggregation => {} sec, delta => {} sec".format(
+                    total_duration, comm_duration, agg_duration, delta_time
+                )
+            )
     finally:
         for w in worker_list:
             w.socket.shutdown(2)
 
+
 def worker_loop(model, dataset, idx, batch_size, epoch, master_port):
-    lr=0.01
-    min_lr=0.001
-    decay_rate=0.97
-    weight_decay=0.0
-    
+    lr = 0.01
+    min_lr = 0.001
+    decay_rate = 0.97
+    weight_decay = 0.0
+
     master_socket = bind_port(ip, master_port)
     config = get_data(master_socket)
     if config == None:
@@ -216,53 +258,87 @@ def worker_loop(model, dataset, idx, batch_size, epoch, master_port):
         master_socket.close()
         exit(1)
 
-    try: 
-        print('Create local model {}...'.format(model))
+    try:
+        print("Create local model {}...".format(model))
         local_model = models.get_model(model)
     except ValueError as e:
         print(e)
         master_socket.close()
         exit(1)
-    
+
     try:
-        print('Load dataset {}...'.format(dataset))
-        train_dataset, test_dataset = datasets.load_dataset(dataset,DATASET_PATH)
+        print("Load dataset {}...".format(dataset))
+        train_dataset, test_dataset = datasets.load_dataset(dataset, DATASET_PATH)
     except ValueError as e:
         print(e)
     else:
-        train_loader = datasets.create_dataloaders(train_dataset, batch_size, selected_idxs=config["train_data_index"])
-        test_loader = datasets.create_dataloaders(test_dataset, batch_size, shuffle=False)
-        
-        torch.nn.utils.vector_to_parameters(config['para'], local_model.parameters())
+        train_loader = datasets.create_dataloaders(
+            train_dataset, batch_size, selected_idxs=config["train_data_index"]
+        )
+        test_loader = datasets.create_dataloaders(
+            test_dataset, batch_size, shuffle=False
+        )
+
+        torch.nn.utils.vector_to_parameters(config["para"], local_model.parameters())
         local_model.to(device)
-        
+
         local_steps = 50
 
         for epoch in range(epoch):
-            start=time.time()
+            start = time.time()
             epoch_lr = max((decay_rate * lr, min_lr))
-            optimizer = torch.optim.SGD(local_model.parameters(), lr=epoch_lr, weight_decay=weight_decay)
-            
-            train_loss = train(local_model, train_loader, optimizer, local_iters=local_steps, device=device)
-            
-            duration=time.time()-start
+            optimizer = torch.optim.SGD(
+                local_model.parameters(), lr=epoch_lr, weight_decay=weight_decay
+            )
+
+            train_loss = train(
+                local_model,
+                train_loader,
+                optimizer,
+                local_iters=local_steps,
+                device=device,
+            )
+
+            duration = time.time() - start
             test_loss, acc = test(local_model, test_loader, device)
-            
-            push_time=time.time()
-            local_para = torch.nn.utils.parameters_to_vector(local_model.parameters()).detach()
+
+            push_time = time.time()
+            local_para = torch.nn.utils.parameters_to_vector(
+                local_model.parameters()
+            ).detach()
             send_timestamp_data(master_socket, push_time, local_para)
 
             updated_para = get_data(master_socket)
             updated_para.to(device)
             torch.nn.utils.vector_to_parameters(updated_para, local_model.parameters())
 
-            print("Epoch {}: train loss = {}, test loss = {}, test accuracy = {}, duration = {} sec.".format(epoch, train_loss,test_loss, acc, duration))
+            print(
+                "Epoch {}: train loss = {}, test loss = {}, test accuracy = {}, duration = {} sec.".format(
+                    epoch, train_loss, test_loss, acc, duration
+                )
+            )
     finally:
         master_socket.shutdown(2)
         master_socket.close()
 
+
 if __name__ == "__main__":
     if args.master == 1:
-        master_loop(args.model, args.dataset, args.worker_num, args.config_file, args.batch_size, args.epoch, args.base_port)
+        master_loop(
+            args.model,
+            args.dataset,
+            args.worker_num,
+            args.config_file,
+            args.batch_size,
+            args.epoch,
+            args.base_port,
+        )
     else:
-        worker_loop(args.model, args.dataset, args.idx, args.batch_size, args.epoch, args.master_port)
+        worker_loop(
+            args.model,
+            args.dataset,
+            args.idx,
+            args.batch_size,
+            args.epoch,
+            args.master_port,
+        )
